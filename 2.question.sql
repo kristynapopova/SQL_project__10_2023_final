c3d0f1526f06cd9b3b28644333f0640abbfdfc8a
kristynapopova <popovakristyna@seznam.cz>
2023-12-04 18:21:30 +0100

    sjednocení dotazů do jednoho

--- 2.question.sql
+++ 2.question.sql
@@ -1,9 +1,6 @@
 
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
-
--- první srovnatelné období 2006: Chléb
- 
 SELECT
 	round(AVG (DISTINCT hruba_mzda), 0),
 	odvětví,
@@ -17,71 +14,14 @@ SELECT
 FROM
 	t_kristyna_popova_project_sql_primary_final tkppspf
 WHERE
-	payroll_year = 2006
-	AND category_code = 111301
+	payroll_year IN (2006,2018)
+	AND category_code IN  (114201,111301)
 GROUP BY
-	odvětví;
-
--- první srovnaltelné období 2006: Mléko
-
-SELECT
-	round(AVG (DISTINCT hruba_mzda), 0),
-	odvětví,
-	payroll_year,
-	potraviny,
 	category_code,
-	cena,
-	množství,
-	round((AVG (DISTINCT hruba_mzda)) / cena) * množství,
-	jednotky
-FROM
-	t_kristyna_popova_project_sql_primary_final tkppspf
-WHERE
-	payroll_year = 2006
-	AND category_code = 114201
-GROUP BY
-	odvětví;
-
-
--- poslední srovnatelné období 2018: Chléb
-
-SELECT
-	round(AVG (DISTINCT hruba_mzda), 0),
 	odvětví,
-	payroll_year,
-	potraviny,
-	category_code,
-	cena,
-	množství,
-	round((AVG (DISTINCT hruba_mzda)) / cena) * množství,
-	jednotky
-FROM
-	t_kristyna_popova_project_sql_primary_final tkppspf
-WHERE
-	payroll_year = 2018
-	AND category_code = 111301
-GROUP BY
-	odvětví;
+	payroll_year;
 
--- poslední srovnatelné období 2018: Mléko
 
-SELECT
-	round(AVG (DISTINCT hruba_mzda), 0),
-	odvětví,
-	payroll_year,
-	potraviny,
-	category_code,
-	cena,
-	množství,
-	round((AVG (DISTINCT hruba_mzda)) / cena) * množství,
-	jednotky
-FROM
-	t_kristyna_popova_project_sql_primary_final tkppspf
-WHERE
-	payroll_year = 2018
-	AND category_code = 114201
-GROUP BY
-	odvětví;
 
 
 

02df5a834b46da3cad551fdfa2eb6d62cae5d276
kristynapopova <popovakristyna@seznam.cz>
2023-12-08 23:38:09 +0100

    čtvrtá otázka edit

--- 4.question.sql
+++ 4.question.sql
@@ -3,30 +3,22 @@
 
 
 SELECT
-roky,
-	round (avg(cena),2)  AS průměrná_cena_potravin,
-	LAG(avg(cena)) OVER(
-	ORDER BY
-	roky ) AS predchozi_radek,
-	round( (avg(cena)- LAG(avg (cena)) OVER(ORDER BY roky ) ) / LAG(avg (cena)) OVER(ORDER BY roky ) * 100, 2 ) AS procentuální_nárůst
-FROM
-	t_kristyna_popova_project_sql_primary_final tkppspf
-GROUP BY
-	roky;
-
-
-SELECT
+	payroll_year,
 	round (avg (hruba_mzda),2) AS průměrná_hrubá_mzda,
-	PAYROLL_YEAR,
 	LAG(avg (hruba_mzda)) OVER(
 	ORDER BY
 	payroll_year ) AS predchozi_radek,
-	round( ( avg (hruba_mzda) - LAG(avg (hruba_mzda)) OVER(ORDER BY PAYROLL_YEAR ) ) / LAG(avg (hruba_mzda)) OVER(ORDER BY payroll_year ) * 100, 2 ) AS procentuální_nárůst
+	round( ( avg (hruba_mzda) - LAG(avg (hruba_mzda)) OVER(ORDER BY payroll_year ) ) / LAG(avg (hruba_mzda)) OVER(ORDER BY payroll_year ) * 100, 2 ) AS procentuální_nárůst_mezd,
+	round (avg(cena),2)  AS průměrná_cena_potravin,
+	LAG(avg(cena)) OVER(
+	ORDER BY
+	payroll_year ) AS predchozi_radek,
+	round( (avg(cena)- LAG(avg (cena)) OVER(ORDER BY payroll_year ) ) / LAG(avg (cena)) OVER(ORDER BY payroll_year ) * 100, 2 ) AS procentuální_nárůst_potravin,
+    round( ( avg (hruba_mzda) - LAG(avg (hruba_mzda)) OVER(ORDER BY payroll_year ) ) / LAG(avg (hruba_mzda)) OVER(ORDER BY payroll_year ) * 100, 2 ) - round( (avg(cena)- LAG(avg (cena)) OVER(ORDER BY payroll_year ) ) / LAG(avg (cena)) OVER(ORDER BY payroll_year ) * 100, 2 ) AS meziroční_rozdíl_v_nárůstu_mezd_a_potravin
 FROM
 	t_kristyna_popova_project_sql_primary_final tkppspf
 GROUP BY
 	payroll_year;
-
 
 	
 
